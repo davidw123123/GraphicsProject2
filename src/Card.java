bb1b96414f9deab195838cb518e17ccfaddc5ffb
BT_2S14_02 <davidwg1210@gmail.com>
2025-04-07 11:02:18 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,131 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.awt.Rectangle;
+
+public class Card {
+    private String suit;
+    private String value;
+    private String imageFileName;
+    private String backImageFileName;
+    private boolean show;
+    private BufferedImage image;
+    private Rectangle cardBox;
+    private boolean highlight;
+    public static ArrayList<Card> cards = new ArrayList<>();
+    public static ArrayList<Card> deck = new ArrayList<>();
+    public static ArrayList<Card> replacedCards = new ArrayList<>();
+
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.imageFileName = "images/card_"+suit+"_"+value+".png";
+        this.show = true;
+        this.backImageFileName = "images/card_back.png";
+
+        // which image should I be showing for the card?
+        // the front image, or the back image
+        this.image = readImage();
+        this.cardBox = new Rectangle(-100, -100, image.getWidth(), image.getHeight());
+        this.highlight = false;
+    }
+
+    public Rectangle getCardBox() {
+        return cardBox;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setRectangleLocation(int x, int y) {
+        cardBox.setLocation(x, y);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
+    }
+
+    public String toString() {
+        return suit + " " + value;
+    }
+
+    public void flipCard() {
+        show = !show;
+        this.image = readImage();
+    }
+
+    public void flipHighlight() {
+        highlight = !highlight;
+    }
+
+    public boolean getHighlight() {
+        return highlight;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    //a BufferedImage object us an object that represents qan image file to be drawn on the screen
+    public BufferedImage readImage() {
+        try {
+            BufferedImage image;
+            // if this is true, show the front
+            // otherwise show the back
+            if (show) {
+                image = ImageIO.read(new File(imageFileName));
+            }
+            else {
+                image = ImageIO.read(new File(backImageFileName));
+            }
+            return image;
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static ArrayList<Card> buildDeck() {
+        ArrayList<Card> deck = new ArrayList<Card>();
+        String[] suits = {"clubs", "diamonds", "hearts", "spades"};
+        String[] values = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "A", "J", "K", "Q"};
+        for (String s : suits) {
+            for (String v : values) {
+                Card c = new Card(s, v);
+                deck.add(c);
+                Card.deck.add(c);
+            }
+        }
+        return deck;
+    }
+
+    public static ArrayList<Card> buildHand() {
+        ArrayList<Card> deck = Card.buildDeck();
+        ArrayList<Card> hand = new ArrayList<Card>();
+        for (int i = 0; i < 9; i++) {
+            int r = (int)(Math.random()*deck.size());
+            Card c = deck.remove(r);
+            hand.add(c);
+            cards.add(c);
+        }
+        return hand;
+    }
+    public static Card randomCard() {
+        for (int i = 0; i < cards.size(); i++) {
+            deck.remove(cards.get(i));
+        }
+            int r = (int)(Math.random()*deck.size());
+            Card c = deck.remove(r);
+            replacedCards.add(c);
+            return c;
+    }
+}
---
+++ src/DrawPanel.java
@@ -0,0 +1,99 @@
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.Font;
+
+class DrawPanel extends JPanel implements MouseListener {
+
+    private ArrayList<Card> hand;
+
+    // Rectangle object represents
+    private Rectangle button;
+
+    public DrawPanel() {
+        button = new Rectangle(145, 260, 160, 26);
+        this.addMouseListener(this);
+        hand = Card.buildHand();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int x = 135;
+        int y = 10;
+        for (int i = 0; i < hand.size(); i++) {
+            Card c = hand.get(i);
+            if (c.getHighlight()) {
+                // draw the border rectangle around the card
+                g.drawRect(x, y, c.getImage().getWidth(), c.getImage().getHeight());
+            }
+            // establish the location of the rectangle "hitbox"
+            c.setRectangleLocation(x, y);
+
+            // puts it in a 3 by 3 grid
+            if (i % 3 == 0 && i != 0)
+            {
+                y = y + c.getImage().getWidth() + 10;
+                x = 135;
+            }
+
+            g.drawImage(c.getImage(), x, y, null);
+            x = x + c.getImage().getWidth() + 10;
+
+
+        }
+
+        // drawing the bottom button
+        // with my favorite font
+        g.setFont(new Font("Courier New", Font.BOLD, 20));
+        g.drawString("GET NEW CARDS", 150, 280);
+        g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+        Point clicked = e.getPoint();
+
+        // left click
+        if (e.getButton() == 1) {
+            // if "clicked" is inside the button rectangle
+            // aka ---> did you click the button?
+            if (button.contains(clicked)) {
+                hand = Card.buildHand();
+            }
+
+            // go through each card
+            // check if any of them were clicked on
+            // if it was clicked, flip the card
+            for (int i = 0; i < hand.size(); i++) {
+                Rectangle box = hand.get(i).getCardBox();
+                if (box.contains(clicked)) {
+                    hand.get(i).flipCard();
+                }
+            }
+        }
+
+        // right click
+        if (e.getButton() == 3) {
+            for (int i = 0; i < hand.size(); i++) {
+                Rectangle box = hand.get(i).getCardBox();
+                if (box.contains(clicked)) {
+                    if (hand.get(i).getHighlight())
+                    {
+                         hand.set(i, Card.randomCard());
+                    }
+                    hand.get(i).flipHighlight();
+                }
+            }
+        }
+
+    }
+
+    public void mouseReleased(MouseEvent e) { }
+    public void mouseEntered(MouseEvent e) { }
+    public void mouseExited(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) { }
+}
